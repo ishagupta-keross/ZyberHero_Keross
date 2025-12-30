1a76aed332efb3f90d33c05380e6f51d883be90d
Isha Kumari Gupta <ishakumarigupta@Keross.local>
2025-12-30 11:45:26 +0530

    Remove large executable file and add to gitignore

--- app/utils/token-management/types.ts
+++
@@ -1,6 +0,0 @@
-export interface TokenResponse {
-  accessToken: string;
-  refreshToken: string; // Usually, refresh endpoints return a new refresh token too
-  expiresIn: number; // Access token expiry in seconds
-  refreshExpiresIn: number; // Refresh token expiry in seconds
-}
